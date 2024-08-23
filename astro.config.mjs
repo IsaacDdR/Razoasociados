0d377b26a2ac4c7a78fdb7492bdd037475245a96
isaacddr <isaacovic00@gmail.com>
2024-08-22 23:05:35 -0600

    added base param astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://isaacddr.github.io",
+  base: "Razoasociados",
   integrations: [tailwind()],
   image: {
    service: passthroughImageService(),
